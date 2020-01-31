6025dd6c746dff935df7696e59c5947744a51adf
franko4don <franko4don@yahoo.com>
2020-01-31 10:35:36 +0100

    cleaned up the initial template

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import Footer from './common/Components/Partials/Footer';
-import Home from './common/Components/Containers/Pages/Home';
 
 class Router extends Component{
 
@@ -15,13 +13,7 @@ class Router extends Component{
             
             <BrowserRouter>
                 <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
-  
-                    <div className="site-wrap">
-                        <Switch>   
-                            <Route exact={'/'}  component={Home}/>
-                        </Switch>
-                        <Footer/>
-                    </div>
+                    <p>hehere</p>
                 </body>                                     
             </BrowserRouter>
             
